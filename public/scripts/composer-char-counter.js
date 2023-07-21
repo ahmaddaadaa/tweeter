00d0c8511988cf8b72b4733e08a2071d7e8b3efd
ahmaddaadaa <ahmadd@uvic.ca>
2023-07-21 00:09:37 -0700

    Add character counter and validation logic for tweet-text input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,23 +1,33 @@
 $(document).ready(function() {
   // --- our code goes here ---
+  
+  $('#alert').hide();
 
   $("#tweet-text").on('input', function() {
-    // console.log(this); //The this keyword is a reference to the button
-    // console.log('button has been pressed!!'); 
+   
     const tweetText = $('#tweet-text').val();
     console.log(tweetText.length);
 
     const lettersRemaining = 140 - tweetText.length;
-    if (lettersRemaining > 0) {
-      $('.counter').html(lettersRemaining);
+    if (lettersRemaining +1 > 0) {
+      $('.counter').html(lettersRemaining).css('color', 'black');
+      $('#alert').empty();
+      $('#alert').hide();
+      
     } else {
+      
       $('.counter').html(lettersRemaining).css('color', 'red');
+      const $meg = $('<h3>Character limit exceeded. Please shorten your message.</h3>');
+      $('#alert').empty().append($meg);
+      $('#alert').show();
+      
     }
 
 
 
   });
 
+  
 
 
 
