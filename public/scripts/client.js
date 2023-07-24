fd1fe22466613ae404efba0029b2b2da177e24bd
ahmaddaadaa <ahmadd@uvic.ca>
2023-07-23 19:59:11 -0700

    Updated to trigger the long characters alert message only upon user submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,12 +3,12 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function() {
+$(document).ready(function () {
   // --- our code goes here ---
 
-  const createTweetElement = function(tweet) {
+  const createTweetElement = function (tweet) {
     function escape(str) {
-      const div = document.createElement('div');
+      const div = document.createElement("div");
       div.textContent = str;
       return div.innerHTML;
     }
@@ -18,14 +18,16 @@ $(document).ready(function() {
         <article>
           <header>
               <div class="picAndUsername">
-                <i><img src="${escape(tweet.user.avatars)}" alt="Icon"></i>
-                <h3>${escape(tweet.user.name)}</h3>
+                <i><img src="${tweet.user.avatars}" alt="Icon"></i>
+                <h3>${tweet.user.name}</h3>
               </div>
-              <h3>${escape(tweet.user.handle)}</h3>
+              <h3>${tweet.user.handle}</h3>
           </header>
               <p>${escape(tweet.content.text)}</p>
           <footer>
-              <time class="timeago" datetime="2023-07-19T12:34:56">${timeago.format(tweet.created_at)}</time>
+              <time class="timeago" datetime="2023-07-19T12:34:56">${timeago.format(
+                tweet.created_at
+              )}</time>
               <div><i class="fa-solid fa-flag"></i>
                 <i class="fa-solid fa-share"></i>
                 <i class="fa-solid fa-heart"></i>
@@ -37,88 +39,77 @@ $(document).ready(function() {
     return $tweet;
   };
 
-
-  const renderTweets = function(tweets) {
+  const renderTweets = function (tweets) {
     // grab the tweets container from the DOM
-    const $container = $('#all-tweets');
+    const $container = $("#all-tweets");
     $container.empty();
 
     for (const tweet of tweets) {
-      // create a tweet box for each element in the array 
+      // create a tweet box for each element in the array
       const $newTweet = createTweetElement(tweet);
 
       // add our new tweet element to the cntainer
       $container.prepend($newTweet);
     }
-
-
   };
 
-
   // loads the tweets
-  const loadTweets = function() {
-
+  const loadTweets = function () {
     $.ajax({
-      method: 'GET',
-      url: '/tweets',
+      method: "GET",
+      url: "/tweets",
       success: (data) => {
-       
         renderTweets(data);
-
       },
-      error: function(error) {
+      error: function (error) {
         // Handle any errors that occur during the AJAX request
-        console.error('Error getting tweet:', error);
-      }
+        console.error("Error getting tweet:", error);
+      },
     });
-
   };
   loadTweets();
 
-
-  
-  $('.new-tweet-form').submit(function(event) {
+  $(".new-tweet-form").submit(function (event) {
     event.preventDefault(); // Prevent the default form submission
-    
+
     // Get the form data
     const formData = $(this).serialize();
-    const tweetText = $('#tweet-text').val().trim();
-    if(tweetText === '' || tweetText === null){
-      const $meg = $('<h3>Empty input. Please type a message.</h3>');
-      $('#alert').empty().append($meg).show();
-      return;
-    };
-
-    // Check if tweetText.length is greater than 140
-    if(tweetText.length > 140){
+    const tweetText = $("#tweet-text").val().trim();
+    if (tweetText === "" || tweetText === null) {
+      const $meg = $("<h3>Empty input. Please type a message.</h3>");
+      $("#alert").empty().append($meg).show();
       return;
     }
-    
+
+    // Check if tweetText.length is greater than 140
+    if (tweetText.length > 140) {
+      const $meg = $(
+        "<h3>Character limit exceeded. Please shorten your message.</h3>"
+      );
+      $("#alert").empty().append($meg);
+      $("#alert").show();
+      return;
+    }
+
     // Reset the counter to 140
-    $('.counter').html(140).css('color', 'black');
-    
+    $(".counter").html(140).css("color", "black");
+
     // Perform the AJAX request to submit the data to the server
     $.ajax({
-      type: 'POST',
-      url: '/tweets',
+      type: "POST",
+      url: "/tweets",
       data: formData,
-      success: function(response) {
+      success: function (response) {
         // Handle the successful response if needed
-        console.log('Tweet submitted successfully:', response);
+        console.log("Tweet submitted successfully:", response);
         loadTweets();
       },
-      error: function(xhr, status, error) {
+      error: function (xhr, status, error) {
         // Handle any errors that occur during the AJAX request
-        console.error('Error submitting tweet:', error);
-      }
+        console.error("Error submitting tweet:", error);
+      },
     });
 
-    $('#tweet-text').val('')
-
+    $("#tweet-text").val("");
   });
-
-
-  
-
-
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,34 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // --- our code goes here ---
-  
-  $('#alert').hide();
 
-  $("#tweet-text").on('input', function() {
-   
-    const tweetText = $('#tweet-text').val();
+  $("#alert").hide();
+
+  $("#tweet-text").on("input", function () {
+    const tweetText = $("#tweet-text").val();
     console.log(tweetText.length);
 
     const lettersRemaining = 140 - tweetText.length;
-    if (lettersRemaining +1 > 0) {
-      $('.counter').html(lettersRemaining).css('color', 'black');
-      $('#alert').empty();
-      $('#alert').hide();
-      
+    if (lettersRemaining + 1 > 0) {
+      $(".counter").html(lettersRemaining).css("color", "black");
+      $("#alert").empty();
+      $("#alert").hide();
     } else {
-      
-      $('.counter').html(lettersRemaining).css('color', 'red');
-      const $meg = $('<h3>Character limit exceeded. Please shorten your message.</h3>');
-      $('#alert').empty().append($meg);
-      $('#alert').show();
-      
+      $(".counter").html(lettersRemaining).css("color", "red");
     }
-
-
-
   });
-
-  
-
-
-
 });
